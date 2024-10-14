72fb99bb1526c95dbda979fcc2a79196a05984fd
Uladzislau <salata.vlad131997@yandex.ru>
2024-10-14 21:25:03 +0300

    22. Циклы и метки

--- js/scrript.js
+++ js/scrript.js
@@ -1,19 +1,11 @@
-const numderOfFilms = +prompt("Сколько фильмов вы посмотрели", "0");
-console.log(numderOfFilms);
-
-const personalMovieDB = {
-	count: numderOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
-
-console.log(personalMovieDB);
-
-const newOfFilms = prompt("Один из последних просмотренных фильмов", "");
-const scoreOfFilms = +prompt("На сколько оцените его", "10");
+let star = "";
+const length = 7;
+for (let i = 0; i < length; i++) {
+	for (let j = 0; j < i; j++) {
+		star += "*";
+	}
+	star += "\n";
+}
+console.log(star);
 
 
-personalMovieDB.movies[newOfFilms] = scoreOfFilms;
-console.log(personalMovieDB);
