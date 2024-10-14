85b56c2b9cbd6dec192e11571f248cf59bb0cdd8
Uladzislau <salata.vlad131997@yandex.ru>
2024-10-14 22:18:38 +0300

    Coding Exercise 3: Задания на использование циклов и условий

--- js/scrript.js
+++ js/scrript.js
@@ -9,3 +9,31 @@ for (let i = 0; i < length; i++) {
 console.log(star);
 
 
+
+
+function firstTask() {
+	for (let i = 5; i < 11; i++) {
+		console.log(i);
+	}
+}
+firstTask()
+
+function secondTask() {
+	for (let i = 20; i > 9; i--) {
+		if (i === 13) {
+			break;
+		}
+		console.log(i);
+	}
+}
+secondTask()
+
+
+thirdTask
+function thirdTask() {
+	for (let i = 2; i < 11; i += 2) {
+
+		console.log(i);
+	}
+}
+thirdTask()
