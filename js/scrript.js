51cd6a867ed4efd6df86322660b8d205b5ab8c59
Uladzislau <salata.vlad131997@yandex.ru>
2024-11-01 21:35:14 +0300

    23. Практика, ч2. Применяем условия и циклы

--- js/scrript.js
+++ js/scrript.js
@@ -1,129 +1,49 @@
-// let star = "";
-// const length = 7;
-// for (let i = 0; i < length; i++) {
-// 	for (let j = 0; j < i; j++) {
-// 		star += "*";
-// 	}
-// 	star += "\n";
-// }
-// console.log(star);
+const numderOfFilms = +prompt("Сколько фильмов вы посмотрели", "0");
+const personalMovieDB = {
+	count: numderOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+// const a = prompt('Один из последних посмотренных фильмов?', ''),
+// 	b = prompt('На сколько оцените его?', ''),
+// 	c = prompt('Один из последних посмотренных фильмов?', ''),
+// 	d = prompt('На сколько оцените его?', '');
 
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-
-
-// function firstTask() {
-// 	for (let i = 5; i < 11; i++) {
-// 		console.log(i);
-// 	}
-// }
-// firstTask()
-
-// function secondTask() {
-// 	for (let i = 20; i > 9; i--) {
-// 		if (i === 13) {
-// 			break;
-// 		}
-// 		console.log(i);
-// 	}
-// }
-// secondTask()
-
-
-// thirdTask
-// function thirdTask() {
-// 	for (let i = 2; i < 11; i += 2) {
-
-// 		console.log(i);
-// 	}
-// }
-// thirdTask()
-
-// fourthTask
-// for (let i = 2; i <= 16; i++) {
-// 	if (i % 2 === 0) {
-// 		continue;
-// 	} else {
-// 		console.log(i);
-// 	}
-// }
-
-// function fourthTask() {
-// 	let result = 1;
-// 	while (result < 14) {
-// 		result += 2;
-// 		console.log(result);
-// 	}
-// }
-// fourthTask()
-
-// fifthTask
-
-// function fifthTask() {
-// 	const arr = [];
-// 	for (let i = 5; i < 11; i++) {
-// 		arr[i - 5] = i
-// 	}
-// 	console.log(arr);
-// 	return arr;
-// }
-// fifthTask()
-
-// Coding Exercise 4: 
-
-// function firstTask() {
-// 	const arr = [3, 5, 8, 16, 20, 23, 50];
-// 	const result = [];
-// 	for (let i = 0; i < arr.length; i++) {
-// 		result[i] = arr[i]
-// 	}
-// 	console.log(result);
-// 	return result;
-// }
-// firstTask()
-
-// function secondTask() {
-// 	const data = [5, 10, 'Shopping', 20, 'Homework'];
-// 	for (let i = 0; i < data.length; i++) {
-// 		let a = data[i];
-// 		if (typeof a === "number") {
-// 			data[i] = data[i] * 2;
-// 		} else {
-// 			data[i] = data[i] + " - done";
-// 		}
-// 	}
-// 	console.log(data);
-// 	return data;
-// }
-// secondTask()
-
-// function thirdTask() {
-// 	const data = [5, 10, 'Shopping', 20, 'Homework'];
-// 	const result = [];
-
-// 	for (let i = 0; i < data.length; i++) {
-
-// 		result[i] = data[data.length - 1 - i];
-// 	}
-// 	console.log(result);
-// 	return result;
-// }
-// thirdTask()
-
-// Coding Exercise 5:
-
-
-
-let star = "";
-const length = 5;
-
-for (let i = 0; i <= length; i++) {
-	for (let j = 0; j < length - i; j++) {
-		star += " ";
+for (let i = 0; i < 1; i++) {
+	const a = prompt('Один из последних посмотренных фильмов?', ''),
+		b = prompt('На сколько оцените его?', '');
+	if ((a || b) === null || (a.length || b.length) === 0 || a.length >= 50) {
+		i = i - 1;
+	} else {
+		personalMovieDB.movies[a] = b;
 	}
-	for (let j = 0; j < 2 * i + 1; j++) {
-		star += "*";
-	}
-	star += "\n";
+
+	//  ||
+
+	// && a || b === undefined && a.length || b.length >= 50
+	// console.log(typeof (a));
+	// console.log(a.length);
+
+	// console.log(a);
+
 
 }
-console.log(star);
+
+if (personalMovieDB.count < 10) {
+	console.log("Посмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+	console.log("Вы киноман");
+} else {
+	console.log("Произошла ошибка");
+}
+
+
+
+console.log(personalMovieDB);
