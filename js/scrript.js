608b08cfa41c46da54569fa70cbcb02d9df181b4
Uladzislau <salata.vlad131997@yandex.ru>
2024-11-03 18:29:40 +0300

    25. (д) Еще раз про аргументы функций

--- js/scrript.js
+++ js/scrript.js
@@ -1,32 +1,13 @@
 "use strict";
 
-let num = 20;
-function showFirstMessage(text) {
-	console.log(text);
-	num = 10;
-	console.log(num);
+
+
+const usdCurr = 20;
+const eurCurr = 20;
+
+
+function convert(amount, curr) {
+	console.log(curr * amount);
 }
-
-showFirstMessage("hello world");
-console.log(num);
-
-// function calc(a, b) {
-// 	return (a + b);
-// }
-// console.log(calc(4, 3));
-// console.log(calc(5, 6));
-
-function ret() {
-	let num = 50;
-	return num;
-}
-const anotherNum = ret();
-console.log(anotherNum);
-
-const logger = function () {
-	console.log('hello world');
-};
-logger();
-
-
-const calc = (a, b) => { return a + b };
+convert(500, usdCurr);
+convert(500, eurCurr);
