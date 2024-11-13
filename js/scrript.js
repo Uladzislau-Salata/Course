23be0933b7a34ff0412d7b5ff05dd9b69fb954e4
Uladzislau <salata.vlad131997@yandex.ru>
2024-11-13 22:04:11 +0300

    Coding Exercise 7: (*) Продвинутые задания на использование функций

--- js/scrript.js
+++ js/scrript.js
@@ -1,67 +1,70 @@
 "use strict";
+// Место для первой функции
+// function calculateVolumeAndArea(numder) {
+// 	if (typeof (numder) != 'string' && numder > 0 && (numder + '').indexOf('.') === -1) {
+// 		console.log(`Объём куба:${numder * numder * numder}, площадь всей поверхности:${numder * numder * 6}`);
+// 	} else {
+// 		console.log("При вычислении произошла ошибка");
+// 	}
+// }
 
-let numberOfFilms;
 
-function start() {
-	numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
-	while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-		numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+function calculateVolumeAndArea(length) {
+	if (typeof (length) !== 'number' || length < 0 || !Number.isInteger(length)) {
+		return console.log("При вычислении произошла ошибка");
 	}
+
+	let volume = 0,
+		area = 0;
+
+	volume = length * length * length;
+	// length ** 3 - это тоже самое, что и выше или варианты через цикл.
+	// ** - это оператор степени, напоминаю. Но онлайн редактор его не принимает =/
+	area = 6 * (length * length);
+
+	console.log(`Объем куба: ${volume}, площадь всей поверхности: ${area}`);
 }
 
-start();
-
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
+calculateVolumeAndArea(5);
+calculateVolumeAndArea(15);
+calculateVolumeAndArea(15.5);
+calculateVolumeAndArea('15');
+calculateVolumeAndArea(-15);
 
 
-function rememberMyFilms() {
-	for (let i = 0; i < 2; i++) {
-		const a = prompt("Один из последних просмотренных фильмов?", ''),
-			b = prompt("На сколько оцените его?", '');
-		if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-			personalMovieDB.movies[a] = b;
-			console.log('done');
-		} else {
-			console.log('error');
-			i--;
-		}
-	}
-}
 
-rememberMyFilms();
 
-function detectPersonalLevel() {
-	if (personalMovieDB.count < 10) {
-		console.log("Просмотрено довольно мало фильмов");
-	} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
-		console.log("Просмотрено довольно мало фильмов");
-	} else if (personalMovieDB.count > 30) {
-		console.log("Вы киноман");
+// Место для второй функции
+function getCoupeNumber(seatNumber) {
+	if (!Number.isInteger(seatNumber) || seatNumber < 0) {
+		return console.log("Ошибка. Проверьте правильность введенного номера места");
+	} else if (seatNumber === 0 || seatNumber > 36) {
+		return console.log("Таких мест в вагоне не существует");
 	} else {
-		console.log("Произошла ошибка");
-	}
-}
-detectPersonalLevel();
-
-
-function showMyDB() {
-	if (personalMovieDB.privat != true) {
-		console.log(personalMovieDB);
+		if (seatNumber % 4 === 0) {
+			return console.log(Math.floor(seatNumber / 4 - 1) + 1);
+		}
+		return console.log(Math.floor(seatNumber / 4) + 1);
 	}
 }
 
-showMyDB();
-
-function writeYourGenres() {
-	for (let i = 1; i <= 3; i++) {
-		personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`, "");
+function getCoupeNumber(seatNumber) {
+	if (typeof (seatNumber) !== 'number' || seatNumber < 0 || !Number.isInteger(seatNumber)) {
+		return "Ошибка. Проверьте правильность введенного номера места";
 	}
-	console.log(personalMovieDB);
+
+	if (seatNumber === 0 || seatNumber > 36) {
+		return "Таких мест в вагоне не существует";
+	}
+
+	return Math.ceil(seatNumber / 4);
+	// тут очень много вариантов решения, но выбрал один из элегантных :)
 }
-writeYourGenres();
+
+getCoupeNumber(33);
+getCoupeNumber(7);
+getCoupeNumber(300);
+getCoupeNumber(0);
+getCoupeNumber(7.7);
+getCoupeNumber(-10);
+getCoupeNumber('Hello');
