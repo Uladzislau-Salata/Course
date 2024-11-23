61ceb25d625bd5e9a41c87ceaf176be11a78a882
Uladzislau <salata.vlad131997@yandex.ru>
2024-11-23 22:56:40 +0300

    Coding Exercise 10: Задачи на работу с объектами

--- js/scrript.js
+++ js/scrript.js
@@ -1,96 +1,44 @@
 "use strict";
 
-// let a = 5,
-// 	b = a;
-// b = b + 5;
+const personalPlanPeter = {
+	name: "Peter",
+	age: "29",
+	skills: {
+		languages: ['ru', 'eng'],
+		programmingLangs: {
+			js: '20%',
+			php: '10%'
+		},
+		exp: '1 month'
+	}, showAgeAndLangs: function (plan) {
+		const { age } = plan;
+		const { languages } = plan.skills;
+		let str = "";
+		for (const key in languages) {
+			str += ` ${languages[key].toUpperCase()}`;
+		}
 
-// console.log(b);
-// console.log(a);
-
-// const obj = {
-// 	a: 5,
-// 	b: 1
-// }
-
-// const copy = obj;
-
-// copy.a = 10;
-// console.log(copy);
-// console.log(obj);
-
-function copy(mainObj) {
-
-	let objCopy = {};
-	let key;
-
-	for (const key in mainObj) {
-		objCopy[key] = mainObj[key];
+		return console.log(`Мне ${age} и я владею языками:${str}`);
 	}
-	return objCopy;
-}
-const numbers = {
-	a: 2,
-	b: 5,
-	c: {
-		x: 7,
-		y: 4
-	}
-}
-
-const newNumbers = copy(numbers);
-
-newNumbers.a = 10;
-newNumbers.c.x = 10;
-// console.log(newNumbers);
-// console.log(numbers);
-
-const add = {
-	d: 17,
-	e: 20
-}
-
-const clone = (Object.assign({}, add));
-clone.d = 20
-// console.log(add);
-// console.log(clone); 
-
-const oldArray = ["a", "b", "c"];
-
-const newArray = oldArray.slice();
-
-
-newArray[1] = "sadfgfgbsda";
-console.log(oldArray);
-console.log(newArray);
-
-const video = ["youtube", "vimeo", "rutube"],
-	blogs = ["wordpress", "livejournal", "blogger"],
-	internet = [...video, ...blogs, "vk", "facebook"];
-
-console.log(internet);
-
-function log(a, b, c) {
-	console.log(a);
-	console.log(b);
-	console.log(c);
-
-}
-
-const num = [2, 5, 7];
-
-log(...num);
-
-
-const array = ["a", "b"];
-const newAarray = [...array];
-
-const q = {
-	one: 1,
-	two: 2
 };
 
-const newObj = { ...q };
+personalPlanPeter.showAgeAndLangs(personalPlanPeter);
 
+function showExperience(plan) {
+	const { exp } = plan.skills;
+	return console.log(exp);
+}
 
+showExperience(personalPlanPeter);
 
+function showProgrammingLangs(plan) {
+	const { programmingLangs } = plan.skills;
+	let str = "";
+	for (const key in programmingLangs) {
+		str += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
+	}
 
+	return console.log(str);
+}
+
+showProgrammingLangs(personalPlanPeter);
