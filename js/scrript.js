1a3bbbdb5fa173b7d1d287ed5b22ad6f8bdbe6b8
Uladzislau <salata.vlad131997@yandex.ru>
2024-12-01 15:01:40 +0300

    Coding Exercise 14: (*) Продвинутая задача на работу с объектами и массивами

--- js/scrript.js
+++ js/scrript.js
@@ -1,49 +1,26 @@
 "use strict";
 
-const shoppingMallData = {
-	shops: [
-		{
-			width: 10,
-			length: 5
-		},
-		{
-			width: 15,
-			length: 7
-		},
-		{
-			width: 20,
-			length: 5
-		},
-		{
-			width: 8,
-			length: 10
-		}
-	],
-	height: 5,
-	moneyPer1m3: 30,
-	budget: 50000
-}
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam', "Somebody"];
 
-function isBudgetEnough(shop) {
-	let storeArea = 0;
-	let storeVolume = 0;
-	let storуHeating = 0;
+function sortStudentsByGroups(arr) {
+	arr.sort();
 
-	for (const element of shop.shops) {
-		for (const key in element) {
-			storeArea += element.width * element.length;
+	let a = [],
+		b = [],
+		c = [],
+		rest = [];
+
+	for (let i = 0; i < arr.length; i++) {
+		if (i < 3) {
+			a.push(arr[i]);
+		} else if (i < 6) {
+			b.push(arr[i]);
+		} else if (i < 9) {
+			c.push(arr[i]);
+		} else {
+			rest.push(arr[i]);
 		}
 	}
-
-	storeVolume = storeArea * shop.height;
-	storуHeating = storeVolume * shop.moneyPer1m3;
-
-
-	if (shop.budget - storуHeating >= 0) {
-		return console.log("Бюджета достаточно");
-	} else {
-		return console.log("Бюджета недостаточно");
-	}
+	return console.log([a, b, c, `Оставшиеся студенты: ${rest.length == 0 ? '-' : rest.join(", ")}`]);
 }
-
-isBudgetEnough(shoppingMallData);
+sortStudentsByGroups(students);
