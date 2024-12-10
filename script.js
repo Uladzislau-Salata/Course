80c3e7002d14bb2852ab574ffd886917e60f36ec
Uladzislau <salata.vlad131997@yandex.ru>
2024-12-10 21:34:13 +0300

    49. Async, defer, динамические скрипты

--- script.js
+++ script.js
@@ -1,33 +1,17 @@
 'use strict';
 
-// touchstart
-// touchmove
-// touchend
-// touchenter
-// touchleave
-// touchcancel
 
-window.addEventListener("DOMContentLoaded", () => {
-	const box = document.querySelector('.box');
-	console.log(box);
-	box.addEventListener('touchstart', (e) => {
-		e.preventDefault();
-		console.log('start');
-		console.log(e.targetTouches);
-	});
-
-	box.addEventListener('touchmove', (e) => {
-		e.preventDefault();
-		console.log(e.targetTouches[0].pageX);
-	});
-	// box.addEventListener('touchend', (e) => {
-	// 	e.preventDefault();
-	// 	console.log('End');
-	// });
+const p = document.querySelectorAll("p");
+console.log(p);
 
 
-});
 
-// touches
-// targetTouches
-// changedTouches
+function loadScript(src) {
+	const script = document.createElement('script');
+	script.src = src;
+	script.async = false;
+	document.body.append(script);
+}
+
+loadScript('test.js');
+loadScript('some.js');
