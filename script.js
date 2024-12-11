be8733f5e963412d51e5d61f595c419062f12e5f
Uladzislau <salata.vlad131997@yandex.ru>
2024-12-11 21:31:18 +0300

    54. (д) Живые коллекции и полезные методы

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 'use strict';
 
-const box = document.querySelector('.box');
-const block = document.querySelector('.block');
+const boxesQuery = document.querySelectorAll('.box');
+const boxesGet = document.getElementsByClassName('box');
 
-console.log(block);
+boxesQuery.forEach(box => {
+	if (box.matches('.this')) console.log(box);
+})
+
+console.log(boxesQuery[0].closest('.wrapper'));
 
 
-// if (block) {
-// 	console.log(block.textContent);
+// boxesQuery[0].remove();
+// boxesGet[0].remove();
+
+// for (let i = 0; i < 5; i++) {
+// 	const div = document.createElement('div');
+// 	div.classList.add('box');
+// 	// document.body.append(div);
+
+// 	boxesGet[boxesGet.length] = div;
 // }
 
-// console.log(block?.textContent);
-// block?.textContent = '123';
-
-// console.log(1 + 2);
-
-const userData = {
-	name: 'Ivan',
-	age: null,
-	say: function () {
-		console.log('Hello');
-	}
-}
-
-userData.say();
-userData.hey?.();
-
-// if (userData && userData.skills && userData.skills.js) {
-// 	console.log(userData.skills.js);
-// }
+// console.log(boxesQuery);
+// console.log(boxesGet);
+// // console.log(document.body.children);
 
 
-console.log(userData?.skills?.js);
+// console.log(Array.from(boxesGet));
+
+
+
