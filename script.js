9456a60e453928a016ec505fb5bdcbbe30689a07
Uladzislau <salata.vlad131997@yandex.ru>
2024-12-11 21:55:20 +0300

    55. (д) Тип данных Symbol

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 'use strict';
 
-const boxesQuery = document.querySelectorAll('.box');
-const boxesGet = document.getElementsByClassName('box');
+// let id = Symbol('id');
 
-boxesQuery.forEach(box => {
-	if (box.matches('.this')) console.log(box);
-})
-
-console.log(boxesQuery[0].closest('.wrapper'));
-
-
-// boxesQuery[0].remove();
-// boxesGet[0].remove();
-
-// for (let i = 0; i < 5; i++) {
-// 	const div = document.createElement('div');
-// 	div.classList.add('box');
-// 	// document.body.append(div);
-
-// 	boxesGet[boxesGet.length] = div;
+// const obj = {
+// 	name: 'Test',
+// 	[Symbol('id')]: 1,
+// 	getId: function () {
+// 		return this[id];
+// 	}
 // }
 
-// console.log(boxesQuery);
-// console.log(boxesGet);
-// // console.log(document.body.children);
+
+// // // console.log(id == id2);
+
+// // obj[id] = 1;
+
+// // console.log(obj.getId());
+// console.log(obj[Object.getOwnPropertySymbols(obj)[0]]);
+
+// // for (let value in obj) {
+// // 	console.log(value);
+// // }
 
 
-// console.log(Array.from(boxesGet));
 
+const myAwesomeDB = {
+	movies: [],
+	actors: [],
+	[Symbol.for('id')]: 123
+}
 
+//  Сторонний код библеотеки
 
+myAwesomeDB.id = "324234234";
+
+console.log(myAwesomeDB[Symbol.for("id")]);
+console.log(myAwesomeDB);
