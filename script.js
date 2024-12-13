0431f575f437741f3c0e42d5ab3e61826e67a1ad
Uladzislau <salata.vlad131997@yandex.ru>
2024-12-13 21:13:52 +0300

    57. (*) Итерируемые конструкции

--- script.js
+++ script.js
@@ -5,26 +5,62 @@ const birthday = Symbol('birthday');
 const user = {
 	name: 'Alex',
 	surName: 'Smith',
-	[birthday]: '20/04/2021',
+	birthday: '20/04/2021',
 	showMyPublicData: function () {
 		console.log(`${this.name} ${this.surName}`);
 	}
 }
-// Object.defineProperty(user, 'birthday', { value: prompt('Date?'), enumerable: true, configurable: true });
+
+for (const key in user) {
+	console.log(user[key]);
+}
 
 
-console.log(Object.getOwnPropertyDescriptor(user, birthday));
-
-Object.defineProperty(user, 'showMyPublicData', { enumerable: false });
-for (let key in user) console.log(key);
-
-Object.defineProperties("user", {
-	name: { writable: false },
-	surname: { writable: false }
-})
+const arr = ['b', 'a', 'c'];
+Array.prototype.someMethod = function () { };
 
 
-//writable
-//enumerable
-//configurable
+// console.dir(arr);
 
+for (const key of arr) {
+	console.log(key);
+}
+
+// const str = 'string';
+
+// for (const key in str) {
+// 	console.log(str[key]);
+// }
+
+const salaries = {
+	john: 500,
+	ivan: 1000,
+	ann: 5000,
+	sayHello: function () {
+		console.log('Hello')
+	}
+}
+
+salaries[Symbol.iterator] = function () {
+	return {
+		current: this.john,
+		last: this.ann,
+		next() {
+			if (this.current < this.last) {
+				this.current = this.current + 500;
+				return { done: false, value: this.current }
+			} else {
+				return { done: true }
+			}
+
+			// { done: true, value: 123 }
+		}
+	}
+}
+
+const iterator = salaries[Symbol.iterator]();
+console.log(iterator.next());
+
+// for (let res of salaries) {
+// 	console.log(res);
+// }
