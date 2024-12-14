cedf47b6695dec96a8e390392a41a7c4e5a5fb91
Uladzislau <salata.vlad131997@yandex.ru>
2024-12-14 14:36:42 +0300

    58. (*) Map

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const birthday = Symbol('birthday');
-
 const user = {
 	name: 'Alex',
 	surName: 'Smith',
@@ -10,57 +8,53 @@ const user = {
 		console.log(`${this.name} ${this.surName}`);
 	}
 }
+const userMap = new Map(Object.entries(user));
+// console.log(userMap);
 
-for (const key in user) {
-	console.log(user[key]);
-}
-
-
-const arr = ['b', 'a', 'c'];
-Array.prototype.someMethod = function () { };
+const newUserObj = Object.fromEntries(userMap);
+console.log(newUserObj);
 
 
-// console.dir(arr);
+const shops = [
+	{ rice: 500 },
+	{ oil: 200 },
+	{ bread: 50 }
+];
+const butget = [5000, 15000, 25000];
 
-for (const key of arr) {
-	console.log(key);
-}
+const map = new Map([
+	[{ paper: 400 }, 8000]
+]);
+shops.forEach((shop, i) => {
+	map.set(shop, butget[i]);
+});
 
-// const str = 'string';
+console.log(map);
+// console.log(map.get(shops[0]));
+// console.log(map.has(shops[0]));
+// map.delete(key);
+// map.clear();
+// map.size();
+// map.keys();
+// console.log(map.keys());
 
-// for (const key in str) {
-// 	console.log(str[key]);
+// const goods = [];
+
+// for (let shop of map.keys()) {
+// 	goods.push(Object.keys(shop)[0]);
+// }
+// console.log(goods);
+
+// for (let price of map.values()) {
+// 	console.log(price);
 // }
 
-const salaries = {
-	john: 500,
-	ivan: 1000,
-	ann: 5000,
-	sayHello: function () {
-		console.log('Hello')
-	}
-}
-
-salaries[Symbol.iterator] = function () {
-	return {
-		current: this.john,
-		last: this.ann,
-		next() {
-			if (this.current < this.last) {
-				this.current = this.current + 500;
-				return { done: false, value: this.current }
-			} else {
-				return { done: true }
-			}
-
-			// { done: true, value: 123 }
-		}
-	}
-}
-
-const iterator = salaries[Symbol.iterator]();
-console.log(iterator.next());
-
-// for (let res of salaries) {
-// 	console.log(res);
+// for (let [shop, price] of map.entries()) {
+// 	console.log(price, shop);
 // }
+
+// map.forEach((value, key, map) => {
+// 	console.log(value, key);
+// })
+
+
