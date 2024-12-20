01122b8fb9a46c6bb96e8da5f321fe9c8e0e0ba8
Uladzislau <salata.vlad131997@yandex.ru>
2024-12-20 19:43:27 +0300

    Coding Exercise 18: (**) Сложная задача на работу со строками

--- script.js
+++ script.js
@@ -1,43 +1,57 @@
 'use strict';
 
-// function amountOfPages(summary) {
-// 	let num = 0;
-
-// 	for (let i = 1; i <= summary; i++) {
-
-// 		if (i < 10) {
-// 			num += String(i).length;
-// 		} else {
-// 			num += String(i).length;
-// 			if (num === summary) {
-// 				num = i;
-// 				break
-// 			}
-// 		}
-
-// 	}
-// 	return num;
-// }
 
 
-
-// amountOfPages(5);
-// amountOfPages(25);
-// amountOfPages(1095);
-// amountOfPages(185);
-
-
-function amountOfPages(summary) {
-	let result = '';
-	let n = 0;
-
-	for (let i = 1; i <= summary; i++) {
-		result += i;
-		if (result.length === summary) {
-			n = i;
-			break;
+function isPangram(string) {
+	const abc = "abcdefghijklmnopqrstuvwxyz";
+	let newabc = '';
+	for (let i = 0; i < abc.length; i++) {
+		if (string.toLowerCase().includes(abc[i])) {
+			newabc += abc.slice(i, i + 1);
 		}
 	}
+	return abc === newabc ? true : false;
+}
+isPangram('The quick brown fox jumps over the lazy dog');
 
-	return n;
+
+// Вариант, когда строка переводится в нижний регистр до всех операций только 1 раз
+// Это должно экономить ресурсы компьютера
+function isPangram(string) {
+	string = string.toLowerCase();
+	return "abcdefghijklmnopqrstuvwxyz".split("").every(function (x) {
+		return string.indexOf(x) !== -1;
+	});
+}
+
+// С другим методом и строка каждый раз преобразовывается в коллбэке
+function isPangram(string) {
+	return 'abcdefghijklmnopqrstuvwxyz'
+		.split('')
+		.every((x) => string.toLowerCase().includes(x));
+}
+
+// При помощи цикла
+function isPangram(str) {
+	letters: for (var c = 0; c < 26; c++) {
+		for (let i = 0; i < str.length; i++) {
+			let s = str.charCodeAt(i)
+			if (s < 65 || s > 90 && s < 97 || s > 122) continue
+			if (s === 65 + c || s === 97 + c) continue letters
+		}
+
+		return false
+	}
+
+	return true
+}
+
+// При помощи Set
+function isPangram(string) {
+	return new Set(string.toLocaleLowerCase().replace(/[^a-z]/gi, '').split('')).size === 26;
+}
+
+// С использованием регулярных выражений
+function isPangram(string) {
+	return (string.match(/([a-z])(?!.*\1)/ig) || []).length === 26;
 }
