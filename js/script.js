3ee9f9a7612bb8ec55fe29bd37abcc419261637a
Uladzislau <salata.vlad131997@yandex.ru>
2024-12-22 21:54:52 +0300

    64. Скрипты и время их выполнения. setTimeout и setInterval

--- js/script.js
+++ js/script.js
@@ -1,43 +1,44 @@
 'use strict';
 
-window.addEventListener('DOMContentLoaded', () => {
-	const tabs = document.querySelectorAll('.tabheader__item'),
-		tabsContent = document.querySelectorAll('.tabcontent'),
-		tabsParent = document.querySelector('.tabheader__items');
 
-	function hideTabContent() {
-		tabsContent.forEach(item => {
-			item.classList.add('hide');
-			item.classList.remove('show', 'fade');
-		})
-		tabs.forEach(item => {
-			item.classList.remove('tabheader__item_active');
-		});
+const btn = document.querySelector('.btn');
+let timerId,
+	i = 0;
 
-	};
+function myAnimation() {
+	const elem = document.querySelector('.box');
+	let pos = 0;
 
-	function showTabContent(i = 0) {
-		tabsContent[i].classList.add('show', 'fade');
-		tabsContent[i].classList.remove('hide');
-		tabs[i].classList.add('tabheader__item_active');
+	const id = setInterval(frame, 10);
 
+	function frame() {
+		if (pos == 300) {
+			clearInterval(id);
+		} else {
+			pos++;
+			elem.style.top = pos + 'px';
+			elem.style.left = pos + 'px';
+		}
 	}
-	hideTabContent();
-	showTabContent();
 
-	tabsParent.addEventListener('click', (e) => {
-		const target = e.target;
-		if (target && target.classList.contains('tabheader__item'))
-			tabs.forEach((item, i) => {
-				if (target == item) {
-					hideTabContent();
-					showTabContent(i);
-				}
-			});
 
-	});
+}
 
-});
+btn.addEventListener('click', myAnimation);
 
 
 
+// function logger() {
+
+// 	if (i === 3) {
+// 		clearInterval(timerId);
+// 	}
+
+// 	console.log('text');
+// 	i++;
+// }
+
+// let id = setTimeout(function log() {
+// 	console.log('Hello');
+// 	id = setTimeout(log, 500);
+// }, 500);
