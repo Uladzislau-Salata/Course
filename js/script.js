0983b3518ca5eb08786f9f7cd929da24ee5e0480
Uladzislau <salata.vlad131997@yandex.ru>
2024-12-23 21:38:00 +0300

    65. (д) Сборщик мусора и утечки памяти

--- js/script.js
+++ js/script.js
@@ -1,44 +1,41 @@
 'use strict';
 
-
-const btn = document.querySelector('.btn');
-let timerId,
-	i = 0;
-
-function myAnimation() {
-	const elem = document.querySelector('.box');
-	let pos = 0;
-
-	const id = setInterval(frame, 10);
-
-	function frame() {
-		if (pos == 300) {
-			clearInterval(id);
-		} else {
-			pos++;
-			elem.style.top = pos + 'px';
-			elem.style.left = pos + 'px';
-		}
-	}
-
-
-}
-
-btn.addEventListener('click', myAnimation);
-
-
-
-// function logger() {
-
-// 	if (i === 3) {
-// 		clearInterval(timerId);
-// 	}
-
-// 	console.log('text');
-// 	i++;
+// function func() {
+// 	window.smth = 'string';
 // }
 
-// let id = setTimeout(function log() {
-// 	console.log('Hello');
-// 	id = setTimeout(log, 500);
-// }, 500);
+// const someRes = getData();
+// const node = document.querySelector('.class');
+// setInterval(function () {
+// 	if (node) {
+// 		node.innerHTML = someRes;
+// 	}
+// }, 1000);
+
+// function outer() {
+// 	const potentiallHugeArray = [];
+// 	return function inner() {
+// 		potentiallHugeArray.push('Hello');
+// 		console.log('Hello!!!!');
+// 	}
+// }
+// const sayHello = outer();
+
+function createElement() {
+	const div = document.createElement('div');
+	div.id = 'test';
+	document.body.append(testDiv);
+	// return div;
+}
+
+createElement();
+// const testDiv = createElement();
+
+
+
+function deleteElem() {
+	document.body.removeChild(document.getElementById('test'));
+}
+
+deleteElem();
+
