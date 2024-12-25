c540b956530b702307f3201afa2d5d732cad79d4
Uladzislau <salata.vlad131997@yandex.ru>
2024-12-25 20:20:29 +0300

    67. Работа с датами

--- js/script.js
+++ js/script.js
@@ -1,46 +1,30 @@
 'use strict';
 
-// let user = { name: 'Ivan' };
-// let map = new WeakMap();
-// map.set(user, 'data');
-// user = null;
+const now = new Date('2024-12-25');
+// new Date.parse('2024-12-25');
 
-// console.log(map);
 
-let cache = new WeakMap();
+console.log(now.setHours(40));
+console.log(now);
 
-function cacheUser(user) {
-	if (!cache.has(user)) {
-		cache.set(user, Date.now());
-	}
-	return cache.get(user);
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
+
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+	let some = i ** 3;
 }
-let lena = { name: "Elena" };
-let alex = { name: "Alex" };
 
-cacheUser(lena);
-cacheUser(alex);
+let end = new Date();
 
-lena = null;
-
-console.log(cache.has(lena));
-console.log(cache.has(alex));
-
-// WeakSet
-// add, has, delete
-
-let messages = [
-	{ text: 'Hello', from: 'John' },
-	{ text: 'World', from: 'Alex' },
-	{ text: '...', from: 'M' },
-];
-
-let readMessages = new WeakSet();
-
-readMessages.add(messages[0]);
-// readMessages.add(messages[1]);
-
-readMessages.add(messages[0]);
-messages.shift();
-console.log(readMessages.has(messages[0]));
-
+alert(`Цикл отработал за ${end - start} миллисекунд`);
