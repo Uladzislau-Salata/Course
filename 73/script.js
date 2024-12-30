ce380542f6df99dc539148f334441f430613a18b
Uladzislau <salata.vlad131997@yandex.ru>
2024-12-30 22:34:46 +0300

    73. (д) MutationObserver, ResizeObserver и contenteditable

---
+++ 73/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const box = document.querySelector('.box');
+
+let observer = new MutationObserver(mutationRecords => {
+
+	console.log(mutationRecords);
+
+});
+
+observer.observe(box, {
+	childList: true
+})
+
+observer.disconnect();
+
