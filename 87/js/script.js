f5613b25f81d07a0d37eb799c515254454afee27
Uladzislau <salata.vlad131997@yandex.ru>
2025-02-01 17:20:13 +0300

    Coding Exercise 20: Задания на работу с методами массивов

--- 87/js/script.js
+++ 87/js/script.js
@@ -1,49 +1,48 @@
 'use strict';
 
-//filter
+const films = [
+	{
+		name: 'Titanic',
+		rating: 9
+	},
+	{
+		name: 'Die hard 5',
+		rating: 5
+	},
+	{
+		name: 'Matrix',
+		rating: 8
+	},
+	{
+		name: 'Some bad film',
+		rating: 4
+	}
+];
 
-// const names = ['Ivan', 'Ann', 'Ksenia', 'Voldemart'];
-
-// const shortNames = names.filter(function (name) {
-// 	return name.length < 5;
-// })
-
-// console.log(shortNames);
-
-//map
-
-// const answers = ['IvAn', 'AnnA', 'Hello'];
-
-// const result = answers.map(item => item.toLowerCase());
-// console.log(result);
-
-//every/some
-
-// const some = [4, 8, 1];
-
-// console.log(some.some(item => typeof (item) === 'number'));
-// console.log(some.every(item => typeof (item) === 'number'));
-
-//reduce
-
-
-// const arr = [3, 4, 5, 6, 6];
-// const result = arr.reduce((sum, current) => sum + current, 3);
-// console.log(result);
-
-// const arr = ['apple', 'pear', 'plum'];
-// const result = arr.reduce((sum, current) => `${sum}, ${current}`);
-// console.log(result);
-
-const obj = {
-	ivan: 'person',
-	ann: 'person',
-	dog: 'animal',
-	cat: 'animal',
+function showGoodFilms(arr) {
+	return arr.filter(item => item.rating >= 8)
 }
 
-const newArr = Object.entries(obj)
-	.filter(item => item[1] === 'person')
-	.map(item => item[0]);
+showGoodFilms(films);
 
-console.log(newArr);
+function showListOfFilms(arr) {
+	return arr.map(item => item.name).reduce((summ, current) => `${summ}, ${current}`
+	)
+}
+showListOfFilms(films);
+
+
+function setFilmsIds(arr) {
+	return arr.map((item, i) => {
+		item.id = i;
+		return item
+	});
+}
+
+const tranformedArray = setFilmsIds(films);
+
+
+function checkFilms(arr) {
+	return arr.every(item => item.id || item.id === 0 ? true : false)
+}
+checkFilms(tranformedArray);
