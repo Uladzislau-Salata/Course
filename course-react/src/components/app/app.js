57905cf7aa3d9d0cb5a94e01aff0a3584456a9f0
Uladzislau <salata.vlad131997@yandex.ru>
2025-02-24 22:06:01 +0300

    130. Самостоятельное задание на работу с состояниями

--- course-react/src/components/app/app.js
+++ course-react/src/components/app/app.js
@@ -6,38 +6,106 @@ import EmployeersAddForm from '../employeers-add-form/employeers-add-form';
 
 import './app.css';
 
-import { Component } from 'react';
+// import { Component } from 'react';
 
-class WhoAmI extends Component {
+// class WhoAmI extends Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			years: 27,
-			text: '+++'
-		}
-	}
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {
+// 			years: 27,
+// 			text: '+++'
+// 		}
+// 	}
 
-	nextYear = () => {
-		console.log('+++');
-		this.setState(state => ({
-			years: state.years + 1
-		}))
-	}
+// 	nextYear = () => {
+// 		console.log('+++');
+// 		this.setState(state => ({
+// 			years: state.years + 1
+// 		}))
+// 	}
 
-	render() {
-		const { name, surname, link } = this.props;
-		return (
-			<div>
-				<button onClick={this.nextYear}>{this.state.text}</button>
-				<h1>My name is {name}, surename - {surname}, age- {this.state.years}</h1>
-				<a href={link}>My profile</a>
-			</div>
-		)
-	}
+// 	render() {
+// 		const { name, surname, link } = this.props;
 
-}
+// 		return (
+// 			<div>
+// 				<button onClick={this.nextYear}>{this.state.text}</button>
+// 				<h1>My name is {name}, surename - {surname}, age- {this.state.years}</h1>
+// 				<a href={link}>My profile</a>
+// 			</div>
+// 		)
+// }
 
+// }
+
+// <WhoAmI name='John' surname='Smith' link="facebook.com" />
+// <WhoAmI name='Alex' surname='Shepard' link="vk.com" />
+
+
+// (class App extends React.Component {
+
+// 	constructor(props) {
+// 	  super(props);
+// 	  this.state={
+// 		value : this.props.counter
+// 	  }
+// 	}
+
+// 	incrementr=()=>{
+// 	  if( this.state.value < 50 ){
+// 		  this.setState(state => ({
+// 			  value: state.value + 1
+// 			}));
+// 	  }
+// 	};
+
+// 	decrement=()=>{
+// 	 if( this.state.value > -50 ){
+// 		  this.setState(state => ({
+// 			  value: state.value - 1
+// 			}));
+// 	   }
+// 	};
+
+// 	random=()=>{
+// 		this.setState(state => ({
+// 			  value: Math.floor(Math.random() * 101) - 50
+// 			}));
+// 	}
+
+// 	reset=()=>{
+// 		this.setState(state => ({
+// 			  value:this.props.counter
+// 			}));
+// 	}
+
+// Используйте только стрелочную форму методов
+// Почему? Подробный ответ будет в следующем уроке
+
+// 	render() {
+
+
+// 	  return (
+// 		<div class="app">
+// 		  <div class="counter">{this.state.value}</div>
+// 		  <div class="controls">
+// 			<button onClick={this.incrementr}>INC</button>
+// 			<button onClick={this.decrement}>DEC</button>
+// 			<button onClick={this.random}>RND</button>
+// 			<button onClick={this.reset}>RESET</button>
+// 		  </div>
+// 		</div>
+// 	  )
+// 	}
+//   }
+
+//   ReactDOM.render(<App counter={10}/>, document.getElementById('app'));
+
+// 1) Начальное значение счетчика должно передаваться через props
+// 2) INC и DEC увеличивают и уменьшают счетчик соответственно на 1. Без ограничений, но можете добавить границу в -50/50. По достижению границы ничего не происходит
+// 3) RND изменяет счетчик в случайное значение от -50 до 50. Конструкцию можете прогуглить за 20 секунд :) Не зависит от предыдущего состояния
+// 4) RESET сбрасывает счетчик в 0 или в начальное значение из пропсов. Выберите один из вариантов)
 
 
 
@@ -55,8 +123,7 @@ function App() {
 	return (
 		<div className="app">
 
-			<WhoAmI name='John' surname='Smith' link="facebook.com" />
-			<WhoAmI name='Alex' surname='Shepard' link="vk.com" />
+
 
 			<AppInfo />
 
