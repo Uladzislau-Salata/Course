5e778d62d13932fd0ebe9f0f30b7b018aaead3d5
Uladzislau <salata.vlad131997@yandex.ru>
2025-03-02 18:20:41 +0300

    137. Практика. Реализуем поиск и фильтры

--- course-react/src/components/app/app.js
+++ course-react/src/components/app/app.js
@@ -16,7 +16,9 @@ class App extends Component {
 				{ name: "John C.", salary: 800, increase: false, rise: true, id: 1 },
 				{ name: "Alex M.", salary: 3000, increase: true, rise: false, id: 2 },
 				{ name: "Carl W.", salary: 15000, increase: false, rise: false, id: 3 },
-			]
+			],
+			term: "",
+			filter: "all"
 		}
 		this.maxId = 4;
 	}
@@ -31,7 +33,6 @@ class App extends Component {
 
 
 	addItem = (name, salary) => {
-		if (!name || !salary) return;
 		const newItem = {
 			name,
 			salary,
@@ -58,12 +59,41 @@ class App extends Component {
 	}
 
 
+	searchEmp = (items, term) => {
+		if (term.length === 0) {
+			return items;
+		}
+		return items.filter(item => {
+			return item.name.indexOf(term) > -1
+		})
+	}
 
+	onUpdateSearch = (term) => {
+		this.setState({ term });
+	}
+
+	filterPost = (items, filter) => {
+		switch (filter) {
+			case "rise":
+				return items.filter(item => item.rise);
+			case "moreThen1000":
+				return items.filter(item => item.salary > 1000);
+			default:
+				return items;
+		}
+	}
+
+	onFilterSelect = (filter) => {
+		this.setState({ filter })
+	}
 
 
 	render() {
+		const { data, term, filter } = this.state;
+
 		const employeer = this.state.data.length;
 		const increased = this.state.data.filter(item => item.increase).length;
+		const visibaleData = this.filterPost(this.searchEmp(data, term), filter);
 
 		return (
 			<div className="app" >
@@ -71,12 +101,15 @@ class App extends Component {
 				<AppInfo employeer={employeer} increased={increased} />
 
 				<div className="search-panel">
-					<SearchPanel />
-					<AppFilter />
+					<SearchPanel
+						onUpdateSearch={this.onUpdateSearch} />
+					<AppFilter
+						filter={filter}
+						onFilterSelect={this.onFilterSelect} />
 				</div>
 
 				<EmployeersList
-					data={this.state.data}
+					data={visibaleData}
 					onDelete={this.deleteItem}
 					onToggleProp={this.onToggleProp} />
 				<EmployeersAddForm
