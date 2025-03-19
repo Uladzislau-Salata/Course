ace821860e0b5fe1178af8990e0eb6f7610ce133
Uladzislau <salata.vlad131997@yandex.ru>
2025-03-19 20:38:12 +0300

    153. Пагинация данных (дозагрузка персонажей)

--- course-react/src/services/MarvelService.js
+++ course-react/src/services/MarvelService.js
@@ -3,6 +3,7 @@ class MarvelService {
 
 	_apiBase = 'https://gateway.marvel.com/v1/public/';
 	_apiKey = 'apikey=2a74b8ec54e23524fad42d9ef7083ae1';
+	_baseOffset = 210;
 
 	getResourse = async (url) => {
 		let res = await fetch(url);
@@ -12,8 +13,8 @@ class MarvelService {
 		return await res.json();
 	}
 
-	getAllCharacters = async () => {
-		const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+	getAllCharacters = async (offset = this._baseOffset) => {
+		const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
 		return res.data.results.map(this._transformCharacter);
 	}
 
