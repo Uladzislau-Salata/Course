d74561500bbab29e56d67709417ae544aa49beaf
Uladzislau <salata.vlad131997@yandex.ru>
2025-03-30 20:13:31 +0300

    168. useRef

--- course-react/src/components/testApp/TestApp.js
+++ course-react/src/components/testApp/TestApp.js
@@ -1,99 +1,43 @@
-import { useState, useCallback, useEffect, useMemo } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import './TestApp.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const countTotal = (num) => {
-    console.log('counting...');
-    return num + 10
-}
+const Form = () => {
+    const [text, setText] = useState('');
 
-const Slider = (props) => {
 
-    const [slide, setSlide] = useState(0);
-    const [autoplay, setAutoplay] = useState(false);
+    const myRef = useRef(1);
 
-    const getSomeImage = useCallback(() => {
-        console.log('fetching');
-        return [
-            "https://img.freepik.com/free-photo/light-trails-buildings_1359-714.jpg?t=st=1743347577~exp=1743351177~hmac=46dd1d0e87ca516a4f7b32f0834dba8a4ce456c336ab04960a89c4acef32e10a&w=900",
-            "https://img.freepik.com/free-photo/glowing-futuristic-digital-design-network-server-generated-by-ai_188544-27277.jpg?t=st=1743347172~exp=1743350772~hmac=87590a49f6e19add466e988b665de2a5de10c25633b7d6bdd121d01ffe1e36ad&w=1060"
-        ]
-    }, [])
-
-    function changeSlide(i) {
-        setSlide(slide => slide + i);
-    }
-
-    function toggleAutoplay() {
-        setAutoplay(autoplay => !autoplay);
-    }
-
-    const total = useMemo(() => {
-        return countTotal(slide)
-    }, [slide]);
-
-    const style = useMemo(() => {
-        return {
-            color: slide > 4 ? 'red' : 'black'
-        }
-    }, [slide])
+    // const focusFirstTI = () => {
+    //     myRef.current.focus();
+    // }
 
     useEffect(() => {
-        console.log('styles!')
-    }, [style]);
+        myRef.current = text;
+    })
 
     return (
         <Container>
-            <div className="slider w-50 m-auto">
-
-                <Slide getSomeImage={getSomeImage} />
-
-                <div className="text-center mt-5">Active slide {slide} <br />
-                    {autoplay ? 'auto' : null}
+            <form className="w-50 border mt-5 p-3 m-auto">
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                    <input onChange={(e) => setText(e.target.value)} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
                 </div>
-                <div style={style} className="text-center mt-5">Total slides: {total}</div>
-                <div className="buttons mt-3">
-                    <button
-                        className="btn btn-primary me-2"
-                        onClick={() => changeSlide(-1)}>-1</button>
-                    <button
-                        className="btn btn-primary me-2"
-                        onClick={() => changeSlide(1)}>+1</button>
-                    <button
-                        className="btn btn-primary me-2"
-                        onClick={toggleAutoplay}>toggle autoplay</button>
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                    <textarea value={myRef.current} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                 </div>
-            </div>
+            </form>
         </Container>
     )
 }
 
-
-const Slide = ({ getSomeImage }) => {
-    const [images, setImages] = useState([]);
-
-    useEffect(() => {
-        setImages(getSomeImage())
-    }, [getSomeImage])
-
+function App() {
     return (
-        <>
-            {images.map((url, i) => <img key={i} className="d-block w-100" src={url} alt="slide" />)}
-        </>
-    )
-}
-
-
-function TestApp() {
-    const [slider, setSlider] = useState(true);
-    return (
-        <>
-            <button onClick={() => setSlider(false)}>Click</button>
-            {slider ? <Slider /> : null}
-        </>
+        <Form />
     );
 }
 
-export default TestApp;
+export default App;
