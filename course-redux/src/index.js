96a3ef82e9534a5232db8895f356af77599d2dec
Uladzislau <salata.vlad131997@yandex.ru>
2025-04-29 18:20:48 +0300

    195. Добавим React в проект

--- course-redux/src/index.js
+++ course-redux/src/index.js
@@ -1,44 +1,22 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { createStore, bindActionCreators } from 'redux';
+import { createStore } from 'redux';
 import reducer from './reducer';
-import * as action from './action';
+import { Provider } from 'react-redux'
+
+import App from './components/App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 const store = createStore(reducer);
 
-const { dispatch, subscribe, getState } = store;
-
-
-const update = () => {
-  document.getElementById('counter').textContent = getState().value;
-}
-
-subscribe(update);
-
-// const bindActionCreator = (creacto, dispatch) => (...args) => {
-//   dispatch(creacto(...args));
-// }
-
-const { inc, dec, rnd } = bindActionCreators(action, dispatch);
-// const decDispath = bindActionCreators(dec, dispatch);
-// const rndDispath = bindActionCreators(rnd, dispatch);
-
-
-document.getElementById('inc').addEventListener('click', inc);
-
-document.getElementById('dec').addEventListener('click', dec);
-
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value);
-});
-
 root.render(
   <React.StrictMode>
     <>
+      <Provider store={store}>
+        <App />
+      </Provider>
 
     </>
   </React.StrictMode>
