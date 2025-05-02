f541010c1807ef2988560c9a38fcbfda59ee094c
Uladzislau <salata.vlad131997@yandex.ru>
2025-05-02 16:59:18 +0300

    199. Правило названия action ДЗ 200. Разбор самых сложных моментов

--- course-redux/src/index.js
+++ course-redux/src/index.js
@@ -1,25 +1,15 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { createStore } from 'redux';
-import reducer from './reducer';
-import { Provider } from 'react-redux'
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 
-import App from './components/App';
+import App from "./components/app/App";
+import store from "./store";
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
+import "./styles/index.scss";
 
-const store = createStore(reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <>
-      <Provider store={store}>
-        <App />
-      </Provider>
-
-    </>
-  </React.StrictMode>
+	<Provider store={store}>
+		<App />
+	</Provider>
 );
--- course-redux/src/test.js
+++
@@ -1,7 +0,0 @@
-let num = 10;
-
-const sum = a => num += a;
-
-console.log(sum(5));
-console.log(sum(5));
-console.log(sum(5));
