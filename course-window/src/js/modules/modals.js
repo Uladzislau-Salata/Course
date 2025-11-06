2499a85f3aea557d7f70a803c1db9577059e3bee
Uladzislau <salata.vlad131997@yandex.ru>
2025-11-06 22:19:49 +0300

    223. Работа с формой-калькулятором, часть 2

--- course-window/src/js/modules/modals.js
+++ course-window/src/js/modules/modals.js
@@ -1,6 +1,6 @@
 
 
-const modals = () => {
+const modals = (state) => {
 	function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
 		const trigger = document.querySelectorAll(triggerSelector),
 			modal = document.querySelector(modalSelector),
@@ -10,6 +10,7 @@ const modals = () => {
 
 		trigger.forEach(item => {
 			item.addEventListener('click', (e) => {
+
 				if (e.target) {
 					e.preventDefault();
 				}
@@ -21,6 +22,7 @@ const modals = () => {
 				modal.style.display = "block";
 				document.body.style.overflow = "hidden";
 				// document.body.classList.add('modal-open');
+
 			});
 		});
 
@@ -59,6 +61,11 @@ const modals = () => {
 	bindModal('.popup_engineer_btn ', '.popup_engineer', '.popup_engineer .popup_close');
 	bindModal('.phone_link', '.popup', '.popup .popup_close');
 	bindModal('.popup_calc_btn', '.popup_calc ', '.popup_calc_close');
+	// if (Object.keys(state).length > 2) {
+	// bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false, true);
+	// } else if (Object.keys(state).length > 4) {
+	// bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false, true);
+	// }
 	bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
 	bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
 	// showModalByTime('.popup', 60000);
